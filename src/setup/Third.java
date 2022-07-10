aacad36b355a4174c4dab61998f8b3bec62ae78f
Joshua Marsh <dixiemoxie@gmail.com>
2022-07-10 11:13:37 -0400

    added my first comment in my third file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -5,11 +5,5 @@ public class Third {
     public void myMethod(){
         System.out.println("Hello to file 3");
     }
-
-    // this is my second commit
-
-    // this is my third commit
-
-
-
+    // this is my first comment
 }
